9cd4330e08458b3c4a6acf961d7827d266987c5c
kaitsuks <37402060+kaitsuks@users.noreply.github.com>
2019-11-12 12:01:43 +0200

    First Commit
    Starting GitHub sharing and version control

---
+++ Assets/AudioTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    //Make a Queue of Audio Clips
+    public List<AudioClip> audioQueue = new List<AudioClip>();
+
+    //Have index of where we are at in the queue
+    int index = 0;
+
+    //Get the One AudioSource
+    new AudioSource audio;
+
+    public AudioClip firstClip;
+    public AudioClip secondClip;
+    public AudioClip thirdClip;
+    public AudioClip fourthClip;
+    public AudioClip fifthClip;
+    public AudioClip sixthClip;
+    public AudioClip seventhClip;
+    public AudioClip eigthClip;
+
+    bool hasEntered = false;
+    // Use this for initialization
+    void Start()
+    {
+        audioQueue.Add(firstClip);
+        audioQueue.Add(secondClip);
+        audioQueue.Add(thirdClip);
+        audioQueue.Add(fourthClip);
+        audioQueue.Add(fifthClip);
+        audioQueue.Add(sixthClip);
+        audioQueue.Add(seventhClip);
+        audioQueue.Add(eigthClip);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        audio = other.GetComponent<AudioSource>();
+        if (other.CompareTag("zone") && hasEntered == false)
+        {
+            hasEntered = true;
+            Debug.Log("Entered Zone");
+            if (!audio.isPlaying)
+            {
+                audio.clip = audioQueue[index];
+                audio.Play();
+                index += 1;
+            }
+        }
+    }
+}
---
+++ Assets/PlaySounds.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySounds : MonoBehaviour {
+
+    public AudioClip otherClip;
+    public AudioClip thirdClip;
+    public AudioClip fourthClip;
+
+    new AudioSource audio;
+
+    IEnumerator Start()
+    {
+        audio = GetComponent<AudioSource>();
+
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = otherClip;
+        audio.Play();
+    }
+
+    public void PlayAllClips()
+    {
+        audio.clip = thirdClip;
+        PlayClip();
+        audio.clip = fourthClip;
+        PlayClip();
+
+    }
+
+    public IEnumerator PlayClip()
+    {
+        Debug.Log("Yritetään soittaa...");
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+    }
+
+    public void PlayClips()
+    {
+        Debug.Log("Täällä ollaan...");
+        audio.Play();
+    }
+
+    public void ShowMe()
+    {
+        Debug.Log("Täällä ollaan...");
+        audio.Play();
+    }
+
+    void Update()
+    {
+        if (!audio.isPlaying)
+        {
+            audio.clip = otherClip;
+            audio.Play();
+        }
+    }
+}
